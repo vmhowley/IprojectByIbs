5ab24dc2b050459d0d0c0a545c046e4569e24499
vmhowley <vmhowleyh@gmail.com>
2025-12-23 15:13:59 -0400

    aa

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Snowfall } from 'react-snowfall';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Snowfall style={{ position: 'fixed' }} />
   </StrictMode>
 );
